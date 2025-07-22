63e37b09a49b24b4c57f08db8c906dec0cd953a2
sajjad85gh <116895143+sajjad85gh@users.noreply.github.com>
2025-07-22 19:01:11 +0330

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 /opt/crave/resync.sh && \
 rm -rf {device,vendor,kernel}/daria; \
 rm -rf device/mediatek/sepolicy_vndr hardware/mediatek; \
-git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git device/mediatek/sepolicy_vndr  -b lineage-22.1 && \
+git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git device/mediatek/sepolicy_vndr -b lineage-22.1 && \
 git clone https://github.com/itisFarzin-Phone/vendor_daria_zahedan -b fourteen vendor/daria/zahedan && \
 git clone https://github.com/sajjad85gh/device_daria_zahedan -b rising device/daria/zahedan && \
 git clone https://github.com/LineageOS/android_hardware_mediatek -b lineage-22.1 hardware/mediatek && \
