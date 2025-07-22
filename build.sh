185d721256855526c874c8d483d24fd566b2d8d1
sajjad85gh <sajjad.ghaderi.1385@gmail.com>
2025-07-22 06:30:40 +0330

    build lmodroid 6 for daria bond

---
+++ build.sh
@@ -0,0 +1,12 @@
+crave run --no-patch -- "
+repo init -u https://github.com/LMODroid/manifest.git -b fifteen-qpr1 --git-lfs -g default,-mips,-darwin,-notdefault && \
+/opt/crave/resync.sh && \
+rm -rf {device,vendor,kernel}/daria; \
+rm -rf device/mediatek/sepolicy_vndr hardware/mediatek; \
+git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git device/mediatek/sepolicy_vndr  -b lineage-22.1 && \
+git clone https://github.com/itisFarzin-Phone/vendor_daria_zahedan -b fourteen vendor/daria/zahedan && \
+git clone https://github.com/sajjad85gh/device_daria_zahedan -b lmo6 device/daria/zahedan && \
+git clone https://github.com/LineageOS/android_hardware_mediatek -b lineage-22.1 hardware/mediatek && \
+git clone https://github.com/DariaRnD/kernel_daria_mt6877 kernel/daria/mt6877 && \
+. build/envsetup.sh && \
+brunch zahedan
