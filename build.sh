4c48e5572a01a1fd592a24cd2085f5c99734af3b
sajjad85gh <116895143+sajjad85gh@users.noreply.github.com>
2025-07-24 20:37:01 +0330

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-repo init -u https://github.com/RisingOS-Revived/android -b fifteen --git-lfs -g default,-mips,-darwin,-notdefault && \
+repo init -u https://github.com/ProjectMatrixx/android.git -b 14.0 --git-lfs -g default,-mips,-darwin,-notdefault && \
 /opt/crave/resync.sh && \
 rm -rf {device,vendor,kernel}/daria; \
 rm -rf device/mediatek/sepolicy_vndr hardware/mediatek; \
-git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git device/mediatek/sepolicy_vndr -b lineage-22.1 && \
+git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git device/mediatek/sepolicy_vndr -b lineage-21 && \
 git clone https://github.com/itisFarzin-Phone/vendor_daria_zahedan -b fourteen vendor/daria/zahedan && \
-git clone https://github.com/sajjad85gh/device_daria_zahedan -b rising device/daria/zahedan && \
-git clone https://github.com/LineageOS/android_hardware_mediatek -b lineage-22.1 hardware/mediatek && \
+git clone https://github.com/sajjad85gh/device_daria_zahedan -b matrixx14 device/daria/zahedan && \
+git clone https://github.com/LineageOS/android_hardware_mediatek -b lineage-21 hardware/mediatek && \
 git clone https://github.com/DariaRnD/kernel_daria_mt6877 kernel/daria/mt6877 && \
 . build/envsetup.sh && \
 brunch zahedan
