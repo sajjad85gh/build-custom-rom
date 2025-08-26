67ce9d878dadbe725a640ad2b44cf77712a83133
Sajjad Ghaderi <116895143+sajjad85gh@users.noreply.github.com>
2025-08-27 00:40:57 +0330

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ repo init --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/man
 rm -rf {device,vendor,kernel}/daria; \
 rm -rf device/mediatek/sepolicy_vndr hardware/mediatek; \
 git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr -b lineage-23.0 && \
-git clone https://github.com/itisFarzin-Phone/vendor_daria_zahedan -b sixteen-qpr0 vendor/daria/zahedan && \
+git clone https://github.com/sajjad85gh/vendor_daria_zahedan -b sixteen-qpr0 vendor/daria/zahedan && \
 git clone https://github.com/sajjad85gh/device_daria_zahedan -b infinity16.0 device/daria/zahedan && \
 git clone https://github.com/LineageOS/android_hardware_mediatek -b lineage-22.2 hardware/mediatek && \
 git clone https://github.com/sajjad85gh/kernel-volla-mt6877 kernel/daria/mt6877 --depth 1 && \
