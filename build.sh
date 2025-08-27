123c129a3fdc37a84624e214f1a8de3c4b272b58
Sajjad Ghaderi <116895143+sajjad85gh@users.noreply.github.com>
2025-08-27 20:16:29 +0330

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 repo init --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 16 -g default,-mips,-darwin,-notdefault && \
 /opt/crave/resync.sh && \
-rm -rf {device,vendor,kernel}/daria; \
-rm -rf device/mediatek/sepolicy_vndr hardware/mediatek; \
-git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr -b lineage-23.0 && \
-git clone https://github.com/sajjad85gh/vendor_daria_zahedan -b sixteen-qpr0 vendor/daria/zahedan && \
-git clone https://github.com/sajjad85gh/device_daria_zahedan -b infinity16.0 device/daria/zahedan && \
-git clone https://github.com/LineageOS/android_hardware_mediatek -b lineage-22.2 hardware/mediatek && \
-git clone https://github.com/sajjad85gh/kernel-volla-mt6877 kernel/daria/mt6877 --depth 1 && \
+#rm -rf {device,vendor,kernel}/daria; \
+#rm -rf device/mediatek/sepolicy_vndr hardware/mediatek; \
+#git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr -b lineage-23.0 && \
+#git clone https://github.com/sajjad85gh/vendor_daria_zahedan -b sixteen-qpr0 vendor/daria/zahedan && \
+#git clone https://github.com/sajjad85gh/device_daria_zahedan -b infinity16.0 device/daria/zahedan && \
+#git clone https://github.com/LineageOS/android_hardware_mediatek -b lineage-22.2 hardware/mediatek && \
+#git clone https://github.com/sajjad85gh/kernel-volla-mt6877 kernel/daria/mt6877 --depth 1 && \
 . build/envsetup.sh && \
 lunch infinity_zahedan-user
-m bacon
+make bacon
