f7cf31e9a3554bec3e0753dd0545833e37193ab1
Sajjad Ghaderi <116895143+sajjad85gh@users.noreply.github.com>
2025-08-28 11:06:31 +0330

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
-repo init --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 16 -g default,-mips,-darwin,-notdefault && \
+repo init -u https://github.com/crdroidandroid/android.git -b 16.0 --git-lfs --no-clone-bundle && \
 /opt/crave/resync.sh && \
 rm -rf {device,vendor,kernel}/daria; \
 rm -rf device/mediatek/sepolicy_vndr hardware/mediatek; \
-git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr -b lineage-23.0 && \
+git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr -b lineage-23.0 device/mediatek/sepolicy_vndr && \
 git clone https://github.com/sajjad85gh/vendor_daria_zahedan -b sixteen-qpr0 vendor/daria/zahedan && \
-git clone https://github.com/sajjad85gh/device_daria_zahedan -b infinity16.0 device/daria/zahedan && \
+git clone https://github.com/sajjad85gh/device_daria_zahedan -b cr16.0 device/daria/zahedan && \
 git clone https://github.com/LineageOS/android_hardware_mediatek -b lineage-22.2 hardware/mediatek && \
-git clone https://github.com/sajjad85gh/kernel-volla-mt6877 kernel/daria/mt6877 --depth 1 && \
+git clone https://github.com/sajjad85gh/kernel-volla-mt6877 kernel/daria/mt6877 && \
 . build/envsetup.sh && \
-lunch infinity_zahedan-user
-make installclean
-m bacon
+brunch zahedan
