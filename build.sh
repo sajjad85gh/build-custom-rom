aa617ed3f03097f877cbd29bc4c4eebf4e061c22
Sajjad Ghaderi <116895143+sajjad85gh@users.noreply.github.com>
2025-09-16 21:48:06 +0330

    Update build.sh

--- build.sh
+++ build.sh
@@ -10,5 +10,5 @@ git clone https://github.com/daria-community/device_daria_zahedan -b lineage-22.
 git clone https://github.com/LineageOS/android_hardware_mediatek -b lineage-22.2 hardware/mediatek && \
 git clone https://github.com/sajjad85gh/kernel-volla-mt6877 kernel/daria/mt6877 && \
 . build/envsetup.sh && \
-lunch lineage_stone-bp2a-userdebug && \
+lunch lineage_zahedan-bp2a-userdebug && \
 mka bakon
