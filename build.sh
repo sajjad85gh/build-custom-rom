702bc2c70ac05777e35af2f86ef534df0e434228
Sajjad Ghaderi <116895143+sajjad85gh@users.noreply.github.com>
2025-09-16 11:37:21 +0330

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 rm -rf .repo/local_manifests
-repo init -u https://github.com/crdroidandroid/android.git -b 16.0 --git-lfs --no-clone-bundle && \
+repo init -u https://github.com/LineageOS/android.git -b lineage-22.2 --git-lfs --no-clone-bundle && \
 /opt/crave/resync.sh && \
 rm -rf {device,vendor,kernel}/daria; \
 rm -rf device/mediatek/sepolicy_vndr hardware/mediatek; \
-git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr -b lineage-23.0 device/mediatek/sepolicy_vndr && \
-git clone https://github.com/sajjad85gh/vendor_daria_zahedan -b sixteen-qpr0 vendor/daria/zahedan && \
-git clone https://github.com/sajjad85gh/device_daria_zahedan -b cr16.0 device/daria/zahedan-unified && \
+git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr -b lineage-22.2 device/mediatek/sepolicy_vndr && \
+git clone https://github.com/daria-community/vendor_daria_zahedan -b lineage-22.2 vendor/daria/zahedan && \
+git clone https://github.com/daria-community/device_daria_zahedan -b lineage-22.2 device/daria/zahedan-unified && \
 git clone https://github.com/LineageOS/android_hardware_mediatek -b lineage-22.2 hardware/mediatek && \
 git clone https://github.com/sajjad85gh/kernel-volla-mt6877 kernel/daria/mt6877 && \
 . build/envsetup.sh && \
