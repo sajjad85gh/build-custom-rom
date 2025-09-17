d6b64377647894ec06fd9bec8cd9378b314331f3
Sajjad Ghaderi <116895143+sajjad85gh@users.noreply.github.com>
2025-09-17 09:48:09 +0330

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ rm -rf .repo/local_manifests
 repo init -u https://github.com/LineageOS/android.git -b lineage-22.2 --git-lfs --no-clone-bundle && \
 /opt/crave/resync.sh && \
 rm -rf {device,vendor,kernel}/daria; \
-rm -rf device/mediatek/sepolicy_vndr hardware/mediatek; \
+rm -rf {device,hardware}/mediatek; \
 git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr -b lineage-22.2 device/mediatek/sepolicy_vndr && \
 git clone https://github.com/daria-community/vendor_daria_zahedan -b lineage-22.2 vendor/daria/zahedan && \
 git clone https://github.com/daria-community/device_daria_zahedan -b lineage-22.2 device/daria/zahedan-unified && \
