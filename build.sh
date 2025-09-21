4dee0e9ac7df7778537efea5a9c3ee11950d2dc8
Sajjad Ghaderi <116895143+sajjad85gh@users.noreply.github.com>
2025-09-22 01:23:10 +0330

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 rm -rf .repo/local_manifests
-repo init -u https://github.com/Ingres-Centre/lineage_23.0.git -b lineage-23.0 --git-lfs --no-clone-bundle && \
+repo init -u https://github.com/LineageOS/android.git -b lineage-22.2 --git-lfs --no-clone-bundle && \
 /opt/crave/resync.sh && \
-cd build/soong && \
-wget -O 0001-soong-HACK-disable-soong_filesystem_creator.patch https://raw.githubusercontent.com/sajjad85gh/build-custom-rom/main/0001-soong-HACK-disable-soong_filesystem_creator.patch && \
-git am 0001-soong-HACK-disable-soong_filesystem_creator.patch && \
-cd - && \
-rm -rf {device,vendor,kernel}/daria; \
+rm -rf {device,vendor,kernel,android/hardware}/nothing; \
 rm -rf {device,hardware}/mediatek; \
-git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr -b lineage-23.0 device/mediatek/sepolicy_vndr && \
-git clone https://github.com/daria-community/vendor_daria_zahedan -b upstream/lineage-22.2 vendor/daria/zahedan && \
-git clone https://github.com/daria-community/device_daria_zahedan -b itisFarzin/testing/lineage-23.0 device/daria/zahedan && \
+git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr -b lineage-22.2 device/mediatek/sepolicy_vndr && \
+git clone https://gitlab.com/nothing-2a/proprietary_vendor_nothing_Pacman -b lineage-22.2 vendor/nothing/Pacman && \
+git clone https://github.com/Nothing-2A/android_device_nothing_Aerodactyl -b lineage-22.2 device/nothing/Aerodactyl && \
 git clone https://github.com/LineageOS/android_hardware_mediatek -b lineage-22.2 hardware/mediatek && \
-git clone https://github.com/daria-community/kernel_volla_mt6877 -b itisFarzin/tmp/lineage-23.0 kernel/daria/mt6877 && \
+git clone https://github.com/Nothing-2A/android_device_nothing_Aerodactyl-kernel -b lineage-22.2 kernel/nothing/mt6886 && \
+git clone https://github.com/Nothing-2A/android_hardware_nothing -b lineage22.2 hardware/nothing && \
 . build/envsetup.sh && \
-lunch lineage_zahedan-bp2a-userdebug && \
+lunch lineage_Pacman-bp1a-userdebug && \
 mka bacon
