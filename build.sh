548e418bfe3fb5c672f83b78c694fa4f94eec7d7
Sajjad Ghaderi <116895143+sajjad85gh@users.noreply.github.com>
2025-09-22 00:22:31 +0330

    Update build.sh

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ rm -rf {device,vendor,kernel}/daria; \
 rm -rf {device,hardware}/mediatek; \
 git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr -b lineage-23.0 device/mediatek/sepolicy_vndr && \
 git clone https://github.com/daria-community/vendor_daria_zahedan -b upstream/lineage-22.2 vendor/daria/zahedan && \
-git clone https://github.com/sajjad85gh/device_daria_zahedan_temp -b itisFarzin/testing/lineage-23.0 device/daria/zahedan && \
+git clone https://github.com/daria-community/device_daria_zahedan -b itisFarzin/testing/lineage-23.0 device/daria/zahedan && \
 git clone https://github.com/LineageOS/android_hardware_mediatek -b lineage-22.2 hardware/mediatek && \
 git clone https://github.com/daria-community/kernel_volla_mt6877 -b itisFarzin/tmp/lineage-23.0 kernel/daria/mt6877 && \
 . build/envsetup.sh && \
