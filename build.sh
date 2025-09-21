82c02c12ad941e66d5a3f63717cbf6b01caacd9b
Sajjad Ghaderi <116895143+sajjad85gh@users.noreply.github.com>
2025-09-22 01:28:13 +0330

    Update build.sh

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ git clone https://gitlab.com/nothing-2a/proprietary_vendor_nothing_Pacman -b lin
 git clone https://github.com/Nothing-2A/android_device_nothing_Aerodactyl -b lineage-22.2 device/nothing/Aerodactyl && \
 git clone https://github.com/LineageOS/android_hardware_mediatek -b lineage-22.2 hardware/mediatek && \
 git clone https://github.com/Nothing-2A/android_device_nothing_Aerodactyl-kernel -b lineage-22.2 kernel/nothing/mt6886 && \
-git clone https://github.com/Nothing-2A/android_hardware_nothing -b lineage22.2 hardware/nothing && \
+git clone https://github.com/Nothing-2A/android_hardware_nothing -b lineage-22.2 hardware/nothing && \
 . build/envsetup.sh && \
 lunch lineage_Pacman-bp1a-userdebug && \
 mka bacon
