323df4945130a64e3b28b3642dff9e3a63fe088d
Sajjad Ghaderi <116895143+sajjad85gh@users.noreply.github.com>
2025-10-02 22:17:02 +0330

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,6 +8,8 @@ MANIFEST_URL="https://github.com/ProjectInfinity-X/manifest.git"
 
 # ── Init repo
 rm -rf .repo/local_manifests
+rm -rf {device,vendor,kernel}/daria
+rm -rf {device,hardware}/mediatek
 repo init -u ${MANIFEST_URL} -b ${ROM_BRANCH} --git-lfs --no-clone-bundle
 
 # ── Clone local_manifests
@@ -17,6 +19,7 @@ git clone https://github.com/daria-community/kernel_volla_mt6877 -b itisFarzin/t
 git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr -b lineage-22.2 device/mediatek/sepolicy_vndr
 git clone https://github.com/LineageOS/android_hardware_mediatek -b lineage-22.2 hardware/mediatek
 git clone https://github.com/sajjad85gh/proprietary_vendor_daria_zahedan -b fifteen-qpr2 vendor/daria/zahedan
+
 # ── Sync
 /opt/crave/resync.sh
 
