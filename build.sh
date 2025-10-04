554bfa6a2ff17e2780b0bc744778873dc9f0a7e9
Sajjad Ghaderi <116895143+sajjad85gh@users.noreply.github.com>
2025-10-04 13:41:22 +0330

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@
 ROM_BRANCH="15.0"
 DEVICE="zahedan"
 MANIFEST_URL="https://github.com/ProjectMatrixx/android.git"
-# LOCAL_MANIFEST_URL="https://github.com/sajjad85gh/local_manifests.git"
+LOCAL_MANIFEST_URL="https://github.com/sajjad85gh/local_manifests.git"
 
 # ── Init repo
 rm -rf .repo/local_manifests
@@ -13,15 +13,16 @@ rm -rf {device,hardware}/mediatek
 repo init -u ${MANIFEST_URL} -b ${ROM_BRANCH} --git-lfs --no-clone-bundle
 
 # ── Clone local_manifests
-# git clone ${LOCAL_MANIFEST_URL} -b main .repo/local_manifests
-git clone https://github.com/daria-community/device_daria_zahedan -b Itis_Sajjad/testing/Matrixx-15-qpr2 device/daria/zahedan-unified
-git clone https://github.com/daria-community/kernel_volla_mt6877 -b itisFarzin/testing/lineage-23.0 kernel/daria/mt6877
-git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr -b lineage-22.2 device/mediatek/sepolicy_vndr
-git clone https://github.com/LineageOS/android_hardware_mediatek -b lineage-22.2 hardware/mediatek
-git clone https://github.com/sajjad85gh/proprietary_vendor_daria_zahedan -b fifteen-qpr2 vendor/daria/zahedan
+git clone ${LOCAL_MANIFEST_URL} -b main .repo/local_manifests
 
-# ── Clone LMODynamicWallpaper
-git clone https://github.com/LMODroid/platform_packages_apps_LMODynamicWallpaper -b fifteen-qpr2 packages/apps/LMODynamicWallpaper
+# git clone https://github.com/daria-community/device_daria_zahedan -b Itis_Sajjad/testing/Matrixx-15-qpr2 device/daria/zahedan-unified
+# git clone https://github.com/daria-community/kernel_volla_mt6877 -b itisFarzin/testing/lineage-23.0 kernel/daria/mt6877
+# git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr -b lineage-22.2 device/mediatek/sepolicy_vndr
+# git clone https://github.com/LineageOS/android_hardware_mediatek -b lineage-22.2 hardware/mediatek
+# git clone https://github.com/sajjad85gh/proprietary_vendor_daria_zahedan -b fifteen-qpr2 vendor/daria/zahedan
+
+# # ── Clone LMODynamicWallpaper
+# git clone https://github.com/LMODroid/platform_packages_apps_LMODynamicWallpaper -b fifteen-qpr2 packages/apps/LMODynamicWallpaper
   
 # ── Sync
 /opt/crave/resync.sh
