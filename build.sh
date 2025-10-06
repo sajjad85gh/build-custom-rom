c7e6b7ad25596b1aa413476acfd23ae1214255ff
Sajjad Ghaderi <116895143+sajjad85gh@users.noreply.github.com>
2025-10-06 11:00:13 +0330

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
-
+rm -rf .repo/local_manifests
 rm -rf {device,vendor,kernel,hardware}/nothing
 rm -rf {device,hardware}/mediatek
 # ── Config
-ROM_BRANCH="16.0"
+ROM_BRANCH="15.0"
 DEVICE="Pacman"
-MANIFEST_URL="https://github.com/crdroidandroid/android.git"
+MANIFEST_URL="https://github.com/ProjectMatrixx/android.git"
 LOCAL_MANIFEST_URL="https://github.com/sajjad85gh/local_manifests.git"
 
 # ── Init repo
-rm -rf .repo/local_manifests
 repo init -u ${MANIFEST_URL} -b ${ROM_BRANCH} --git-lfs --no-clone-bundle
 
 # ── Clone local_manifests
@@ -23,11 +22,6 @@ rm -rf hardware/lineage/interfaces/sensors
 PATCHES=(
   "packages/apps/Aperture|36c9507ecf2a1a798d2e7931d9019bacc3cc6052|https://github.com/Nothing-2A/android_packages_apps_Aperture"
   "hardware/lineage/compat|60729c841a8b447896aa8108d2c0cfc0a5327041|https://github.com/LineageOS/android_hardware_lineage_compat"
-  "frameworks/base|79b3ae0b06ffdbadde3d2106a2bbf895b074ffb2|https://github.com/Nothing-2A/android_frameworks_base"
-  "system/core|8ff6e7a68523c3b870d8dcd5713c71ea15b43dd2|https://github.com/Nothing-2A/android_system_core"
-  "system/core|0d5990a96c5e6a404887f5575c5d00bcbbaaef74|https://github.com/Nothing-2A/android_system_core"
-  "frameworks/base|f89e8fa592233d86ad2cabf81df245c4003587cb|https://github.com/AxionAOSP/android_frameworks_base"
-  "frameworks/base|6909a748157404e9150586b9c0860fdb81dd54cc|https://github.com/AxionAOSP/android_frameworks_base"
 )
 
 echo "[*] Applying all patches automatically..."
@@ -73,4 +67,4 @@ export BUILD_HOSTNAME=crave
 # ── Build crDroid
 source build/envsetup.sh
 make installclean
-brunch ${DEVICE} user
+brunch ${DEVICE} userdebug
