c66ff49d64d3c25fb2082d73aa32cb5612896fdc
Sajjad Ghaderi <116895143+sajjad85gh@users.noreply.github.com>
2026-05-08 14:02:27 +0330

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # ── Config
-ROM_BRANCH="lineage-22.2"
-DEVICE="zahedan"
-MANIFEST_URL="https://github.com/LineageOS/android.git"
+ROM_BRANCH="16.2"
+DEVICE="crownlte"
+MANIFEST_URL="https://github.com/Lunaris-AOSP/android.git"
 LOCAL_MANIFEST_URL="https://github.com/sajjad85gh/local_manifests.git"
 
 # ── Clean
@@ -13,28 +13,28 @@ rm -rf {device,hardware}/mediatek
 
 # ── Init repo
 repo init -u ${MANIFEST_URL} -b ${ROM_BRANCH} --git-lfs --no-clone-bundle
-git clone ${LOCAL_MANIFEST_URL} -b main .repo/local_manifests
+git clone ${LOCAL_MANIFEST_URL} -b Exynos9810/Lunaris .repo/local_manifests
 
 # ── Sync
 /opt/crave/resync.sh
 
 # ── Apply patch
-cd build/soong
-wget -O 0001-soong-HACK-disable-soong_filesystem_creator.patch \
-  https://raw.githubusercontent.com/sajjad85gh/build-custom-rom/main/0001-soong-HACK-disable-soong_filesystem_creator.patch
-git am 0001-soong-HACK-disable-soong_filesystem_creator.patch
-cd -
+# cd build/soong
+# wget -O 0001-soong-HACK-disable-soong_filesystem_creator.patch \
+#   https://raw.githubusercontent.com/sajjad85gh/build-custom-rom/main/0001-soong-HACK-disable-soong_filesystem_creator.patch
+# git am 0001-soong-HACK-disable-soong_filesystem_creator.patch
+# cd -
 
 # ── Include KernelSU-Next
 # cd kernel/daria/mt6877
 # curl -LSs "https://raw.githubusercontent.com/KernelSU-Next/KernelSU-Next/next/kernel/setup.sh" | bash -
 # cd -
 
-# ── export
+# ── export  
 export BUILD_USERNAME=Sajjad
 export BUILD_HOSTNAME=crave
 
 # ── build
 . build/envsetup.sh 
-lunch lineage_${DEVICE}-bp1a-userdebug
+lunch lineage_${DEVICE}-bp4a-userdebug
 m bacon
